da682553a7da9594a8567055a2a9119ec924d3d5
Jose Aguilar <jaguilarny@gmail.com>
2022-09-08 14:55:16 -0400

    commit tree 65f932ed9cfd7d02db3c6183e20578fae1ddf19c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def validate_triangle
+    a = @length1
+    b = @length2
+    c = @length3
+
+    raise TriangleError unless
+      a > 0 && b > 0 && c > 0
+    raise TriangleError unless
+      a + b > c && b + c > a && a + c > b
+    
+      return true
+  end
+
+  def kind
+    if validate_triangle
+      case [@length1, @length2, @length3].uniq.length
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Triangle is not valid!"
+    end
+  end
 end
